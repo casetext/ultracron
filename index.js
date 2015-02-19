69c02fb442cf2b1f765987b4d2855026281fef1e
Josh Dague <daguej@email.uc.edu>
2015-02-19 15:54:40 -0800

    Reschedule jobs

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ exports.add = function(id, schedule, fn) {
 		throw new Error('Already have a job named ' + id);
 	}
 
-	var invoker = later.setInterval(function() {
+	jobs[id] = {
+		fn: fn,
+		runs: 0,
+		fails: 0
+	};
+
+	setSchedule(id, schedule);
+	run(id);
+};
+
+function setSchedule(id, schedule) {
+	jobs[id].schedule = later.schedule(schedule);
+	jobs[id].invoker = later.setInterval(function() {
 		if (!jobs[id].paused) {
 			run(id);
 		}
 	}, schedule);
-
-	jobs[id] = {
-		schedule: later.schedule(schedule),
-		fn: fn,
-		runs: 0,
-		invoker: invoker
-	};
-
-	run(id);
-
-};
+}
 
 exports.remove = function(id) {
 	if (!jobs[id]) {
@@ -48,6 +50,16 @@ exports.resume = function(id) {
 	updateJob(id);
 };
 
+exports.reschedule = function(id, opts) {
+	var job = jobs[id];
+	job.invoker.clear();
+
+	var schedule = later.parse.text(opts.schedule);
+	setSchedule(id, schedule);
+	job.next = job.schedule.next(2)[1].valueOf();
+	updateJob(id);
+};
+
 exports.run = run;
 
 function run(id) {
@@ -59,7 +71,12 @@ function run(id) {
 
 	updateJob(id);
 
-	job.fn();
+	job.fn(function(err) {
+		if (err) {
+			job.fails++;
+			updateJob(id);
+		}
+	});
 }
 
 function updateJob(id) {
@@ -77,13 +94,21 @@ comms.on('connection', function(socket) {
 });
 
 
-['pause', 'resume', 'run'].forEach(function(cmd) {
-	comms.on(cmd, function(msg) {
-		var rex = new RegExp(msg.id);
-		for (var job in jobs) {
-			if (rex.test(job)) {
-				exports[cmd](job);
+['pause', 'resume', 'run', 'reschedule'].forEach(function(cmd) {
+	comms.on(cmd, function(msg, socket) {
+		try {
+			if (msg.id == '*') {
+				msg.id = '.+';
 			}
+			var rex = new RegExp(msg.id);
+			for (var job in jobs) {
+				if (rex.test(job)) {
+					exports[cmd](job, msg);
+				}
+			}
+		} catch (ex) {
+			console.error('Error running received command', ex);
+			socket.write({msg:'err', err:ex.toString()});
 		}
 	});
 });
